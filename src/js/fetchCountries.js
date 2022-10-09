efafc658fa7272f5502ccbdb747bd9e243d717e0
Ivan <104319381+IvanDovhopol@users.noreply.github.com>
2022-10-09 05:42:26 +0300

    fix

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,6 +3,11 @@ const filterResponse = 'fields=name,capital,population,flags,languages';
 
 export function fetchCountries(countryName) {
   return fetch(`${BASE_URL}${countryName}?${filterResponse}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
     .catch(console.log);
 }
